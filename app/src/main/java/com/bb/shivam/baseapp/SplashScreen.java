3ee1e8a21b9bf77fa7c4af7e3499c6311892b53c
Shivam Goel <shvmgyl15@hotmail.com>
2017-06-26 18:11:23 +0530

    Added Main Activity

--- app/src/main/java/com/bb/shivam/baseapp/SplashScreen.java
+++ app/src/main/java/com/bb/shivam/baseapp/SplashScreen.java
@@ -1,7 +1,10 @@
 package com.bb.shivam.baseapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
